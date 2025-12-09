8ca4623a74dadba9d742c577d1d7f438c18f3ddd
Yiannis Dimitrakopoulos <ydimitra@f1r2s19.codam.nl>
2025-12-09 20:37:37 +0100

    initial upload

---
+++ ex1/ft_plot_area.py
@@ -0,0 +1,17 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_plot_area.py                                    :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ydimitra <ydimitra@student.42.f>           +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/12/08 18:42:36 by ydimitra          #+#    #+#              #
+#    Updated: 2025/12/08 18:52:07 by ydimitra         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def ft_plot_area():
+    length = int(input("Enter length: "))
+    width = int(input("Enter width: "))
+    area = length * width
+    print(f"Plot area: {area}")
---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,20 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_garden_summary.py                               :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ydimitra <ydimitra@student.42.f>           +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/12/09 09:44:08 by ydimitra          #+#    #+#              #
+#    Updated: 2025/12/09 09:47:58 by ydimitra         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def ft_garden_summary():
+    name = input("Enter garden name: ")
+    plants = input("Enter number of plants: ")
+    print(f"Garden: {name}")
+    print(f"Plants: {plants}")
+    print("Status: Growing well!")
+
+ft_garden_summary()
