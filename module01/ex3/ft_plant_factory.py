e5224c0d5d87853484379120511bf7b5089246c3
Yiannis Dimitrakopoulos <ydimitra@f0r1s9.codam.nl>
2025-12-11 11:55:27 +0100

    create planty of flowers using the class from the prev ex

---
+++ module01/ex3/ft_plant_factory.py
@@ -0,0 +1,33 @@
+class Plant:
+    def __init__(self, name, height, agee, growth):
+        self.name = name
+        self.height = int(height)
+        self.agee = int(agee)
+        self.growth = int(growth);
+
+    def grow(self):
+        if self.name == "Rose":
+            self.height += 1
+        elif self.name == "Sunflower":
+            self.height += 4
+        else:
+            self.height += 0.5
+
+    def age(self):
+        self.agee += 1
+
+def ft_create(flower_name, height, agee, growth):
+    p = Plant(flower_name, height, agee, growth)
+    print(f"Created: {p.name} ({p.height}cm {p.agee} days)")
+    return 1
+
+if __name__ == "__main__":
+    count = 0
+print("=== Plant Factory Output ===")
+count += ft_create("Rose", 25, 30, 0)
+count += ft_create("Oak", 200, 365, 0)
+count += ft_create("Cactus", 5, 90, 0)
+count += ft_create("Sunflower", 80, 45, 0)
+count += ft_create("Fern", 15, 120, 0)
+
+print(f"\nTotal plants created: {count}")
