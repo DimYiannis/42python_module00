c5664515c2ed191bcc295c6eb1e4a54c7484ba78
Yiannis Dimitrakopoulos <ydimitra@f0r1s11.codam.nl>
2025-12-13 17:43:58 +0100

    first ex in mod2

---
+++ module02/ex0/ft_first_exception.py
@@ -0,0 +1,24 @@
+def check_temperature(temp_str):
+    print("=== Garden Temperature Checker ===")
+    
+    try:
+        t = int(temp_str)
+        if t > 40:
+            print(f"Error: {t}°C is too hot for plants (max 40°C)")
+        elif t < 0:
+            print(f"Error: {t}°C is too cold for plants (min 0°C)")
+        else:
+            print(f"Temperature {t}°C is perfect for plants")
+    
+    except ValueError:
+        print(f"Error: '{temp_str}' is not a valid number ")
+    finally:
+        print(f"\nAll tests completed - program didn't crash!")
+
+"""
+check_temperature(10)
+check_temperature(100)
+check_temperature("abc")
+check_temperature(-10)
+
+"""
