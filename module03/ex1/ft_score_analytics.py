c92f94e9a1d2b6b0a4998173ee9ac95667c00f74
Yiannis Dimitrakopoulos <ydimitra@f0r1s10.codam.nl>
2025-12-14 22:26:01 +0100

    appending args to scores

--- module03/ex1/ft_score_analytics.py
+++ module03/ex1/ft_score_analytics.py
@@ -4,7 +4,9 @@ print("=== Player Score Analytics ===")
 if len(sys.argv) == 1:
     print("No scores provided. Usage: python3 ft_score_analytics.py <score2> <score2> ...")
 else:
-    scores = [int(arg) for arg in sys.argv[1:]]
+    scores = []
+    for arg in sys.argv[1:]:
+        scores = scores + [int(arg)]
     print(f"Scores processed: {scores}")
     print(f"Total players: {len(scores)}")
     print(f"Total score: {sum(scores)}")
