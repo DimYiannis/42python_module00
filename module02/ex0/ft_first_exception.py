1949d45a0615255c12083c4459b551a4a5eded6e
Yiannis Dimitrakopoulos <ydimitra@f0r1s10.codam.nl>
2025-12-15 16:51:00 +0100

    got rid of finaly fixed the logic and flake8

--- module02/ex0/ft_first_exception.py
+++ module02/ex0/ft_first_exception.py
@@ -1,25 +1,33 @@
+"""
+we use try except bloke to check if the
+value of temp
+"""
+
+
 def check_temperature(temp_str):
     print("=== Garden Temperature Checker ===")
 
     try:
         t = int(temp_str)
+        print(f"Testing temperature: {t}")
         if t > 40:
-            print(f"Error: {t}°C is too hot for plants (max 40°C)")
+            print(f"Error: {t}°C is too hot for plants (max 40°C)\n")
         elif t < 0:
-            print(f"Error: {t}°C is too cold for plants (min 0°C)")
+            print(f"Error: {t}°C is too cold for plants (min 0°C)\n")
         else:
-            print(f"Temperature {t}°C is perfect for plants")
+            print(f"Temperature {t}°C is perfect for plants\n")
 
     except ValueError:
-        print(f"Error: '{temp_str}' is not a valid number ")
-    finally:
-        print("\nAll tests completed - program didn't crash!")
+        print(f"Error: '{temp_str}' is not a valid number \n")
 
 
-"""
-check_temperature(10)
-check_temperature(100)
-check_temperature("abc")
-check_temperature(-10)
+def test_temperature_input():
 
-"""
+    check_temperature(25)
+    check_temperature(100)
+    check_temperature("abc")
+    check_temperature(-10)
+    print("All tests completed - program didn't crash!")
+
+
+test_temperature_input()
