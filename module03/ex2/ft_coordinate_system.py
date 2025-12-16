79fc305927959a2fd149bd174d7e13ebf9b4cf90
Yiannis Dimitrakopoulos <ydimitra@f1r2s1.codam.nl>
2025-12-16 21:32:13 +0100

    final push

--- module03/ex2/ft_coordinate_system.py
+++ module03/ex2/ft_coordinate_system.py
@@ -2,7 +2,6 @@ import sys
 import math
 
 
-
 def create_pos(x, y, z):
     coord = ()
     try:
@@ -14,7 +13,7 @@ def create_pos(x, y, z):
     except ValueError as e:
         print(f"Parsing invalid coordinates: {x},{y},{z}")
         print(f"Error parsing coordinates: {e}")
-        print(f"Error details - Type: {type(e).__name__}, Args: {e.args}")
+        print(f"Error details - Type: {type(e).__name__}, Args: {e.args}\n")
         return None
 
 
@@ -29,24 +28,24 @@ if __name__ == "__main__":
     x1 = 0
     y1 = 0
     z1 = 0
-    
-    print(f"Position created: {coord}\n")
-    print(f"Parsed position: ({x2}, {y2}, {z2})\n")
-    res = math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
-    print(f"Distance between (0, 0, 0) and ({coord}): {res}")
-    
-    new_coord = create_pos(sys.argv[1][0], sys.argv[1][1], sys.argv[1][2])
-    x2 = new_coord[0]
-    y2 = new_coord[1]
-    z2 = new_coord[2]
 
-    print(f"Parsing coordinates: {sys.argv}")
-    print(f"Parsed position: ({x2}, {y2}, {z2})\n")
-    print(f"Distance between (0, 0, 0) and ({sys.argv}): {res}")
+    print(f"Position created: {coord}")
+    res = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+    print(f"Distance between (0, 0, 0) and ({coord}): {res}\n")
+    res = float(res)
 
-    last_coord = create_pos("abc" ,"def" ,"ghi" )
+    new_coord_str = sys.argv[1]
+    x2, y2, z2 = new_coord_str.split(",")
+    new_coord = create_pos(x2, y2, z2)
+    res = math.sqrt((int(x2) - x1)**2 + (int(y2) - y1)**2 + (int(z2) - z1)**2)
+    res = float(res)
 
-    print("Unpacking demonstration:\n")
-    print(f"Player at x={x2}, y={y2}, z={z2}\n")
+    print(f"Parsing coordinates: {sys.argv[1]}")
+    print(f"Parsed position: ({x2}, {y2}, {z2})")
+    print(f"Distance between (0, 0, 0) and ({new_coord_str}): {res}\n")
+
+    last_coord = create_pos("abc", "def", "ghi")
+
+    print("Unpacking demonstration:")
+    print(f"Player at x={x2}, y={y2}, z={z2}")
     print(f"Coordinates: X={x2}, Y={y2}, Z={z2}")
-
