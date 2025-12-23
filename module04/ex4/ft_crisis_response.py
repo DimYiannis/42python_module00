b44ead9b6ac7d934679e9466dffb4db138664edf
Yiannis Dimitrakopoulos <ydimitra@f0r1s12.codam.nl>
2025-12-23 18:11:29 +0100

    fixes

--- module04/ex4/ft_crisis_response.py
+++ module04/ex4/ft_crisis_response.py
@@ -1,38 +1,36 @@
-def crisis_handler(filename, routine=False):
+def crisis_handler(filename, is_ok=False):
     """
     - Handle archive access during routine operations or crisis situations.
     - open and read file and report whether access is ok or
         triggered by a crisis, and handles common failure.
     """
     try:
-        if routine:
+        if is_ok:
             print(f"ROUTINE ACCESS: Attempting access to '{filename}'...")
         else:
             print(f"CRISIS ALERT: Attempting access to '{filename}'...")
 
-        with open(filename, "r") as vault:
-            print(f"SUCCESS: Archive recovered - ``{vault.read()}``")
+        with open(filename, "r") as data:
+            print(f"SUCCESS: Archive recovered - ``{data.read()}``")
             print("STATUS: Normal operations resumed")
 
     except FileNotFoundError:
         print("RESPONSE: Archive not found in storage matrix")
-        print("STATUS: Crisis handled, system stable")
+        print("STATUS: Crisis handled, system stable\n")
 
     except PermissionError:
         print("RESPONSE: Security protocols deny access")
-        print("STATUS: Crisis handled, security maintained")
+        print("STATUS: Crisis handled, security maintained\n")
 
     except Exception:
         print("RESPONSE: Unexpected system anomaly detected")
-        print("STATUS: Crisis contained, analysis required")
+        print("STATUS: Crisis contained, analysis required\n")
 
 
 if __name__ == "__main__":
     print("=== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM ===\n")
 
     crisis_handler("lost_archive.txt")
-    print("")
     crisis_handler("classified_vault.txt")
-    print("")
-    crisis_handler("standard_archive.txt", routine=True)
-    print("\nAll crisis scenarios handled successfully. Archives secure.")
+    crisis_handler("standard_archive.txt", is_ok=True)
+    print("All crisis scenarios handled successfully. Archives secure.")
